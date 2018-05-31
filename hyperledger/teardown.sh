981f3c23abfccae7292c11f9e235758e55991eb0
Cem Basoglu <cem.basoglu@web.de>
2018-06-01 00:40:09 +0200

    remove chaincode containers on teardown

--- hyperledger/teardown.sh
+++ hyperledger/teardown.sh
@@ -13,6 +13,9 @@ docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.ym
 # remove the local state
 rm -f ~/.hfc-key-store/*
 
+# remove chaincode container
+docker rm $(docker ps -a -q --filter="name=dev-*")
+
 # remove chaincode docker images
 docker rmi $(docker images dev-* -q)
 
